ce7dc2d70a2cc7bf9df03498f08361351d6fb58b
mrtnbroder <mrtnbroder@gmail.com>
2016-11-19 10:21:26 +0100

    refactor(Start-App): rename to Program
    BREAKING CHANGE: you now have to import { program, beginnerProgramm } instead of { runApp, StartApp } from elmux.

--- src/Program.js
+++ src/Program.js
@@ -1,15 +1,15 @@
 
 /**
- *  Start-App.js
+ *  Program.js
  */
 
-import { Observable } from 'rxjs'
+import { Observable as O } from 'rxjs'
 import * as Signal from './Signal'
 import * as Cmd from './Cmd'
 import * as Task from './Task'
 import { reduce, map, flip, nth, curry } from 'ramda'
 
-export const StartApp = (config) => {
+export const program = (config) => {
   const messages = Signal.Mailbox([])
   const singleton = (msg) => [msg]
   const address = Signal.forwardTo(messages.address, singleton)
@@ -19,7 +19,7 @@ export const StartApp = (config) => {
     return [newModel, Cmd.batch([accumulatedCmds, additionalCmds])]
   }
   const update = (msgs, [model]) => reduce(updateStep, [model, Cmd.none])(msgs)
-  const subscriptions = Observable.merge(messages.signal, map(singleton, config.subscriptions))
+  const subscriptions = O.merge(messages.signal, map(singleton, config.subscriptions))
   const cmdsAndModel = subscriptions.startWith(config.init).scan(flip(update)).publishReplay().refCount()
   const model = cmdsAndModel.map(nth(0))
   const html = model.map(curry(config.view)(address))
@@ -32,4 +32,4 @@ export const StartApp = (config) => {
   }
 }
 
-export const runApp = (app) => Observable.merge(app.model, app.html, app.tasks).subscribe()
+export const beginnerProgram = (app) => O.merge(app.model, app.html, app.tasks).subscribe()
